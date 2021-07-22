a3e48ed26d570c919e3533db57e5cc6651323d52
Sadhika Akula <insadhikabox@gmail.com>
2021-07-22 13:23:27 -0700

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Millie Huang on 7/20/21.
+//  Copyright © 2021 Millie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
